0927d4535e35fd14e28ec36cfbd3e3d7fe267b10
Brennan Connor <brennanconnor@gmail.com>
2020-11-01 20:25:54 -0500

    removed useless struct

--- src/filereader/filereader.go
+++ src/filereader/filereader.go
@@ -5,10 +5,6 @@ import (
 	"os"
 )
 
-type openFile struct {
-	contents []string
-}
-
 func openFileForReading(fileName string) (*os.File, error) {
 	file, err := os.Open(fileName)
 	return file, err
